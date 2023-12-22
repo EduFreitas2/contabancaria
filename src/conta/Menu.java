ec0404c526d3cbcfce57e70b11feae2fa68dbe7d
EduFreitas2 <efnascimento.filho2@gmail.com>
2023-12-22 01:14:49 -0300

    CRUD DELETAR

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -70,7 +70,7 @@ public class Menu {
 			}
 			switch (opcao) {
 				case 1:
-						System.out.println(Cores.TEXT_WHITE_BOLD+ "Criar conta\n\n");
+						System.out.println(Cores.TEXT_WHITE+ "Criar conta\n\n");
 						
 						System.out.println("Digite o Número da Agência: ");
 						agencia = read.nextInt();
@@ -102,36 +102,85 @@ public class Menu {
 						keyPress();
 						break;
 				case 2:
-					System.out.println(Cores.TEXT_WHITE_BOLD + "Listar todas as Contas\n\n");
+					System.out.println(Cores.TEXT_WHITE + "Listar todas as Contas\n\n");
 					contas.listarTodas();
 					keyPress();
 					break;
 				case 3:
-					System.out.println(Cores.TEXT_WHITE_BOLD + "Consultar dados da Conta - por número\n\n");
+					System.out.println(Cores.TEXT_WHITE + "Consultar dados da Conta - por número\n\n");
+					System.out.println("\n Digite o número da conta: ");
+					numero = read.nextInt();
+					contas.procurarPorNumero(numero);
 					keyPress();
 					break;
 				case 4:
-					System.out.println(Cores.TEXT_WHITE_BOLD +"Atualizar dados da Conta\n\n");
+					System.out.println(Cores.TEXT_WHITE +"Atualizar dados da Conta\n\n");
+					System.out.println("Atualizar dados da Conta \n\n");
+					
+					System.out.println("Digite o número da conta: ");
+					numero = read.nextInt();
+				
+					var buscaConta = contas.buscarNaCollection(numero);
+					
+					if(buscaConta != null) {
+						
+						
+						tipo = buscaConta.getTipo();
+						
+						System.out.println("Digite o Número da Agência: ");
+						agencia = read.nextInt();
+						System.out.println("Digite o Nome do Titular");
+						read.skip("\\R?");
+						titular = read.nextLine();
+						
+						System.out.println("Digite o Saldo da Conta (R$): ");
+						saldo = read.nextFloat();
+						
+						switch (tipo) {
+						case 1 -> {
+							System.out.println("Digite o Limite de Crédito (R$)");
+							limite = read.nextFloat();
+							
+							contas.atualizar(new ContaCorrente(numero, agencia, tipo, titular, saldo, limite));							
+							}
+						case 2 -> {
+							
+							System.out.println("Digite o dia do Aniversário da Conta: ");
+							aniversario = read.nextInt();
+							contas.atualizar(new ContaPoupanca(numero, agencia, tipo, titular, saldo, aniversario));							
+						}
+						default -> {
+							System.out.println("Tipo de conta inválido!");
+						}
+					}
+					}
+
+					else 
+						System.out.println("A Conta não foi encontrada");
+						
 					keyPress();
 					break;
 				case 5:
-					System.out.println(Cores.TEXT_WHITE_BOLD+"Apagar a Conta\n\n");
+					System.out.println(Cores.TEXT_WHITE+"Apagar a Conta\n\n");
+					System.out.println("Digite o número da conta: ");
+					numero = read.nextInt();
+					contas.deletar(numero);
 					keyPress();
 					break;
 				case 6:
-					System.out.println(Cores.TEXT_WHITE_BOLD+"Saque\n\n");
+					System.out.println(Cores.TEXT_WHITE+"Saque\n\n");
 					keyPress();
 					break;
 				case 7:
-					System.out.println(Cores.TEXT_WHITE_BOLD+"Depósito\n\n");
+					System.out.println(Cores.TEXT_WHITE+"Depósito\n\n");
 					keyPress();
 					break;
 				case 8:
-					System.out.println(Cores.TEXT_WHITE_BOLD+"Transferência entre Contas\n\n");
+					System.out.println(Cores.TEXT_WHITE+"Transferência entre Contas\n\n");
 					keyPress();
 					break;
 				default:
-					System.out.println(Cores.TEXT_WHITE_BOLD+"\n Opção Inválida!\n");
+					System.out.println(Cores.TEXT_WHITE+"\n Opção Inválida!\n");
 					keyPress();
 					break;
 					
